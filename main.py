789a0ed8709f5c0e4a20c4d05fbd2ee9af8baefe
scan3ls <scan3ls@gmail.com>
2021-04-27 20:32:18 +0000

    Add jsonify to flask imports

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flask_cors import (CORS, cross_origin)
 from os import environ
 from uuid import uuid4
