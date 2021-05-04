3862071736b16e411806dbad8e379f4394df83c2
jicruz96 <accounts@jicruz.com>
2021-05-04 02:05:46 +0000

    Fix typo in User class (extra underscore in __tablename__)

--- models/user.py
+++ models/user.py
@@ -4,7 +4,7 @@ from models.base_model import DBase, BaseModel
 
 class User(BaseModel, DBase):
     """ User model """
-    ___tablename__ = 'users'
+    __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
