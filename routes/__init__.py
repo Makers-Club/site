02dd718c857a938d7618499b6571090f619ecaf8
scan3ls <scan3ls@gmail.com>
2021-05-21 23:55:48 +0000

    #46 Add user blueprint and routes

--- routes/__init__.py
+++ routes/__init__.py
@@ -2,6 +2,8 @@ from flask import Blueprint
 
 landing = Blueprint('landing', __name__, url_prefix="")
 auth = Blueprint('auth', __name__, url_prefix="/auth")
+users = Blueprint('users', __name__, url_prefix="/users")
 
 from routes.landing import *
 from routes.auth import *
+from routes.users import *
