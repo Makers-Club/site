b1d86efaa968a555906ac1909cd954c210c17201
jicruz96 <accounts@jicruz.com>
2021-05-21 01:04:07 +0000

    Clean up routes/auth.py

--- routes/auth.py
+++ routes/auth.py
@@ -1,5 +1,5 @@
 """Defines all methods and routes related to user authentication"""
-from flask import request, render_template, Blueprint, abort, redirect, url_for
+from flask import request, render_template, Blueprint, abort, redirect, url_for, make_response
 from requests import get, post # warning: deprecated
 from os import environ
 from re import search
@@ -31,13 +31,7 @@ def send_visitor_to_github():
 
 @auth.route('/github_callback', methods=['GET'], strict_slashes=False)
 def github_callback():
-    """The Github sign-in redirects to this page with a temporary code in the
-    query parameters. This function uses the code to get the user's profile
-    information from Github to sign the user in.
-
-    Return:
-        * dash.html template or the correct error page
-    """
+    """What's our docstring policy"""
     
     gh_tmp_code = request.args.get('code')
     if gh_tmp_code is None:
@@ -48,50 +42,29 @@ def github_callback():
     if user is None:
         # Future coders, address all edge cases! Check get_user() comments
         return 'No verified emails, buddy! Verify your GitHub email.'
-    from flask import make_response
-    response = make_response(render_template('dash.html', data=user.to_dict()))
+    response = make_response(redirect('/'))
+    # response = make_response(render_template('dash.html', data=user.to_dict()))
     response.set_cookie('session', session.id)
     return response
 
 def get_user(gh_tmp_code):
-    """Finds a user profile using Github OAuth temporary code.
-    
-    1. First we get_gh_access_token()
-    2. Then we use gh_access_token to get_gh_user_data()
-    3. Then we find a user that matches the data and return the user identifier (email)
-
-    Args:
-        * gh_tmp_code (str): code provided by GitHub callback used to request access token
-    
-    Return:
-        * user_email (str): user identifier
-    """
+    """receives github code, returns user and session"""
     
     gh_access_token = get_gh_access_token(gh_tmp_code)
     if gh_access_token is None:
-        # either the code was fake or our Github credentials are off.
+        print('Github credentials are off or the code was fake')
         return None, None
     user_data = get_gh_user_data(gh_access_token)
     if user_data is None:
         # User has a Github account without any verified emails
         return None, None
-    # we will redirect to /signup around here to separate github identity verification from registration
-    # this way registration is can be tested with a test token.
-    # Later on this will be a User object, not a user email
     user = match_user(user_data)    
     from models.auth import Auth
     session = Auth.login(gh_access_token, user.id)
     return user, session
 
 def get_gh_access_token(gh_tmp_code):
-    """Exchanges a temporary code for an access token from Github API
-    
-    Args:
-        * gh_tmp_code (str): Github temporary sign-in code
-    
-    Return:
-        * gh_access_token (str): Github access token or None on failure
-    """
+    """Exchanges a temporary code for an access token from Github API"""
 
     # Make POST request to Github Login OAuth Access Token URL
     auth_data = {
@@ -112,14 +85,7 @@ def get_gh_access_token(gh_tmp_code):
     return gh_access_token
 
 def get_gh_user_data(gh_access_token):
-    """Uses a Github access token to retrieve user data
-
-    Args:
-        * gh_access_token (str): Github access token
-    
-    Return:
-        * gh_response.json() (dict): Github user data as a dictionary
-    """
+    """Uses a Github access token to retrieve user data"""
 
     headers = {
         'content-type': 'application/json',
@@ -144,15 +110,7 @@ def get_gh_user_data(gh_access_token):
 
 
 def match_user(user_data):
-    """matches the user data returned by the Github API with our known users.
-    
-    Args:
-        * user_data (list): The JSON response from the GitHub API call for the user's data.
-                            As of now this is A LIST OF EMAIL DICTIONARIES.
-    Return:
-        * (str): Email as a string (FOR NOW)* Later it will be more user data
-    """
-    user = None
+    """matches user data with a user, returns user"""
     from models.user import User
     d = user_data
     user = User.get_by_id(d['id'])
