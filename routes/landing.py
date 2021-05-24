ac6ede052d03ad30c5e34658d884a825c648403e
Russell Molimock <495@holbertonschool.com>
2021-05-24 16:43:12 +0000

    normalized the way we pass current user into routes

--- routes/landing.py
+++ routes/landing.py
@@ -5,17 +5,24 @@ from routes import landing
 @landing.route('/', methods=['GET'], strict_slashes=False)
 def index():
   """ render landing template """
-
+  data = {}
   if request.current_user:
-    return render_template('dash.html', data=request.current_user.to_dict())
-  return render_template('landing.html', data=None)
+    data['current_user'] = request.current_user.to_dict()
+    return render_template('dash.html', data=data)
+  return render_template('landing.html', data=data)
 
 @landing.route('/about', methods=['GET'], strict_slashes=False)
 def about():
   """ render about template """
-  return render_template('about.html', data=None)
+  data = {}
+  if request.current_user:
+    data['current_user'] = request.current_user.to_dict()
+  return render_template('about.html', data=data)
 
 @landing.route('/contact', methods=['GET'], strict_slashes=False)
 def contact():
   """ render contact template """
-  return render_template('contact.html', data=None)
+  data = {}
+  if request.current_user:
+    data['current_user'] = request.current_user.to_dict()
+  return render_template('contact.html', data=data)
