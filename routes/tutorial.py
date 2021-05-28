05df954333776a7ddac4dea9342fd8efe81b52fd
scan3ls <scan3ls@gmail.com>
2021-05-28 23:43:24 +0000

    #48 Add login_required; Fix logic checking quiz; Check answers if passed

--- routes/tutorial.py
+++ routes/tutorial.py
@@ -1,34 +1,45 @@
 from flask import render_template, request
 from routes import tutorial
+from models.auth import auth_client
 
 @tutorial.route('/', methods=['GET', 'POST'], strict_slashes=False)
+@auth_client.login_required
 def index():
     """ starting tutorial page """
     data = {
         'quiz': getQuiz(),
-        'passed': False
+        'passed': False,
+        'current_user': request.current_user
     }
     if request.method == 'GET':
         return render_template('tutorial.html', data=data)
     else:
         quiz = data['quiz']
-        for question in quiz:
-            for option in quiz[question]['options']:
+        questions = quiz['questions']
+        correct_answers = 0
+
+        for question in questions:
+            for option in questions[question]['options']:
                 user_answer = request.form.get(f'{question}_{option}')
 
                 if user_answer is None:
                     continue
 
                 print("user answer: ", user_answer)
-                if user_answer == str(quiz[question]['correct_answer']):
-                    data['passed'] = True
+                if user_answer == str(questions[question]['correct_answer']):
+                    correct_answers += 1
+                    print('Failed on question', question, option)
 
+        data['passed'] = correct_answers == quiz['numberOfCorrectAnswers']
+        print("passed ", data['passed'])
         return render_template('tutorial.html', data=data)
 
 def getQuiz():
     """ placeHolder for getting Quiz questions """
     return {
-        'question1ID': {
+        'numberOfCorrectAnswers': 3,
+        'questions': {
+            'question1ID': {
             'text': "This is a question?",
             'options': {
                 'option1': 1,
@@ -38,25 +49,26 @@ def getQuiz():
             },
             'correct_answer': 1
             
-        },
-        'question2ID': {
-            'text': "This is a question?",
-            'options': {
-                'option1': 1,
-                'option2': 2,
-                'option3': 3,
-                'option4': 4,
             },
-            'correct_answer': 3
-        },
-        'question3ID': {
-            'text': "This is a question?",
-            'options': {
-                'option1': 1,
-                'option2': 2,
-                'option3': 3,
-                'option4': 4,
+            'question2ID': {
+                'text': "This is a question?",
+                'options': {
+                    'option1': 1,
+                    'option2': 2,
+                    'option3': 3,
+                    'option4': 4,
+                },
+                'correct_answer': 3
             },
-            'correct_answer': 2
+            'question3ID': {
+                'text': "This is a question?",
+                'options': {
+                    'option1': 1,
+                    'option2': 2,
+                    'option3': 3,
+                    'option4': 4,
+                },
+                'correct_answer': 2
+            }
         }
     }
