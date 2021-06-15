b9c451bc1b4265df410c7a1b8c4f99b205ff31e4
jicruz96 <accounts@jicruz.com>
2021-06-14 22:37:47 -0400

    Debug checker route and pretty print the checker output

--- routes/checker.py
+++ routes/checker.py
@@ -2,6 +2,7 @@ from flask import jsonify, request
 from requests import post
 from routes import api
 from os import environ
+from pprint import pformat
 
 @api.route('/checker', methods=['POST'], strict_slashes=False)
 def checker():
@@ -9,4 +10,4 @@ def checker():
     if response.status_code != 200:
         print('FAILURE: {}'.format(response.text))
         return jsonify({'string': 'OUR CHECKER API IS DOWN'})
-    return response.json()
+    return jsonify({'string': pformat(response.json())})
