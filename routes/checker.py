4d101ab0899089a9f8fc817fd466f491052665ab
jicruz96 <accounts@jicruz.com>
2021-06-15 00:54:47 -0400

    Basic hello world checker example complete

--- routes/checker.py
+++ routes/checker.py
@@ -2,12 +2,11 @@ from flask import jsonify, request
 from requests import post
 from routes import api
 from os import environ
-from pprint import pformat
 
 @api.route('/checker', methods=['POST'], strict_slashes=False)
 def checker():
     response = post(environ['CHECKER_API_URL'], data=request.form)
     if response.status_code != 200:
         print('FAILURE: {}'.format(response.text))
-        return jsonify({'string': 'OUR CHECKER API IS DOWN'})
-    return jsonify({'string': pformat(response.json())})
+        return jsonify({'checks': None})
+    return jsonify({'checks': response.json()})
--- static/scripts/check.js
+++ static/scripts/check.js
@@ -4,7 +4,50 @@ function check(user, repo) {
         type: 'post',
         data: {user: user, repo: repo},
         dataType: 'json',
-        success: function (data) { $("#target").html( data.string ) },
-        fail: function(jqXHR, text, error) { alert(text) }
+        success: display_checks,
+        fail: function(jqXHR, text, error) { console.log(text) }
     });
 };
+
+/**
+ * From J.I. to others:
+ * This function is solely meant to serve as an example for how to use the
+ * incoming `checks` list from the Checker API. Please create something better
+ * than this.
+ */
+function display_checks(data) {
+    
+    let checks = data.checks;
+    let str = 'Results:';
+    
+    /**
+     * `checks` is a list of `check` dictionaries
+     * 
+     * this is the schema for the `check` dictionary:
+     * {
+     *  'description': str # check description
+     *  'result': bool # True if check passed, False if check failed
+     *  'message': str # message for user
+     *  'check_id': int # check number
+     *  'check_type': str # e.g. requirement, output, pytest, etc
+     *  'stdout': str # checker output
+     *  'stderr': str # checker error
+     * }
+     * 
+     * if no checks were returned, the checker API is down
+     */
+
+    if (!checks) {
+        $("#checks").html('Something went wrong on our end. Retry later.');
+        return;
+    }
+    console.log(checks);
+    /* Here's one example for how you could use some of its attributes */
+    for (let check of checks) {
+        str += `</br>
+        Check ${check.check_id}: ${check.description}
+        ${check.result ? '✅' : '❌'} --- ${check.message}
+        `;
+    }
+    $("#checks").html(str);
+}
