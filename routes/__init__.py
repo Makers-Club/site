88693f0b0ea8e7aaddf0e0248def37b93f74910b
Russell Molimock <495@holbertonschool.com>
2021-06-18 22:44:05 +0000

    payment module and route

--- routes/__init__.py
+++ routes/__init__.py
@@ -5,6 +5,7 @@ auth = Blueprint('auth', __name__, url_prefix="/auth")
 users = Blueprint('users', __name__, url_prefix="/users")
 tutorial = Blueprint('tutorial', __name__, url_prefix="/tutorial")
 projects = Blueprint('projects', __name__, url_prefix="/projects")
+payments = Blueprint('payments', __name__, url_prefix="/payments")
 api = Blueprint('api', __name__, url_prefix="/api")
 
 from routes.landing import *
@@ -12,4 +13,4 @@ from routes.auth import *
 from routes.users import *
 from routes.tutorial import *
 from routes.projects import *
-from routes.checker import *
+from routes.payments import *
