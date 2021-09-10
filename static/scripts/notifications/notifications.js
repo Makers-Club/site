e6eff2833ce8ce6c3caaced111172ac6ce46ee97
scan3ls <scan3ls@gmail.com>
2021-09-10 23:16:35 +0000

    Make good

--- static/scripts/notifications/notifications.js
+++ static/scripts/notifications/notifications.js
@@ -1,36 +1,24 @@
-import { getUser, getElements, mockPing } from './utils.js';
+import { getUser, getElements } from './utils.js';
 
 const user = getUser();
 const { access_token, avatar, credits, email, handle, id, name, projects, roles_of_interest } = user;
 const url = `https://api.makerteams.org/notifications/${id}?token=${access_token}`;
 const { list, button, close, container } = getElements();
 
-const mock = () => {
-    const res = mockPing(id, handle, access_token);
-    const data = res.json();
-    const ul = $('#notification_list ul');
-
-    for (const item of data) {
-        if (item.is_read === "true") continue;
-        const msg = item.msg;
-        const li = $('<li>').append($(msg));
-        ul.append(li);
-    }
-};
-
 $(document).ready(() => {
-    // fetch(url, (res) => {
-    //     const data = res.json();
-    //     const ul = $('#notification_list ul');
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        const { results } = data;
+        const ul = $('#notification_list ul');
 
-    //     for (const item of data) {
-    //         if (item.is_read === "true") continue;
-    //         const msg = item.msg;
-    //         const li = $('<li>').append($(msg));
-    //         ul.append(li);
-    //     }   
-    // });
-    mock();
+        for (const item of results) {
+            if (item.is_read) continue;
+            const msg = item.msg;
+            const li = $('<li>').append($(msg));
+            ul.append(li);
+        }   
+    });
 });
 
 
