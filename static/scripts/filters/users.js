e3872c169a42802db72f1f79178e99bd8f78e7af
scan3ls <scan3ls@gmail.com>
2021-09-15 19:43:06 +0000

    Revert Changes

--- static/scripts/filters/users.js
+++ static/scripts/filters/users.js
@@ -1,33 +1,28 @@
-const roles = [
-    'Front-End',
-    'Back-End',
-];
-let filter_values = ['all'];
-
-function handleOnChange(value) {
-    if (filter_values.includes(value)) filter_values.splice(filter_values.indexOf(value), 1);
-    else filter_values.push(value);
-
-    updateUsers(filter_values);
-}
-
-function updateUsers(filter_values) {
+/**
+ * Event Handler for radio buttons when filtering users
+ * @param {String} filter_value String value of a users chosen role 
+ * Ex. 'Front-End' or 'both'
+ */
+function handleOnChange(filter_value) {
     const users = $('section div.user-card');
+    
     for (const user of users) {
-        const userRoles = user.getAttribute('data-role').split(':');
+        user.style.display = 'none';
+        const userRoles = user.getAttribute('data-role').split(':');  // ex. 'Front-End:Back-End' => ['Front-End', 'Back-End']
 
-        if (filter_values.length === 0) {
-            user.style.display = 'none';
-            continue;
-        }
-        if (filter_values.includes('all')) {
+        if (filter_value === 'all') {
             user.style.display = 'block';
             continue;
         }
 
-        user.style.display = 'none';
+        if (filter_value === 'both') {
+            const hasBothRoles = (userRoles.includes('Front-End') && userRoles.includes('Back-End'));
+            if (hasBothRoles) user.style.display = 'block';
+            continue;
+        }
+
         for (const role of userRoles) {
-            if (filter_values.includes(role)) {
+            if (filter_value === role) {
                 user.style.display = 'block';
                 break;
             }
@@ -35,29 +30,43 @@ function updateUsers(filter_values) {
     }
 }
 
-// list is an ul element
+/**
+ * Create & Load list Elements as user filter options to the page
+ * @param {HTMLUListElement} list Destination UL element to hold list items
+ */
 export function loadUsersFilter(list) {
-    const inputs = [
-        {
-            label: $('<label class="m-0" for="cb_show_all">Show All</label>'),
-            input: $('<input class="ml-1 filter_item" type="checkbox" id="cb_show_all" value="all" checked />')
-        }
-    ];
-    inputs[0].input.change(() => { handleOnChange('all') });
+    // Create radio buttons
+    const showAll = {
+        label: $('<label class="m-0 col-6 p-0" for="rb_show_all">Show All</label>'),
+        input: $('<input class="ml-1 filter_item" type="radio" name="role" id="rb_show_all" value="all" checked />')
+    };
 
-    for (const role of roles) {
-        const role_id = `cb_${role}`;
-        const item = {
-            label: $(`<label class="m-0" for="${role_id}">${role}</label>`),
-            input: $(`<input class="filter_item ml-1" type="checkbox" id="${role_id}" value="${role}" />`)
-        };
+    const showFrontEnd = {
+        label: $(`<label class="m-0 col-6 p-0" for="rb_Front-End">Front-End</label>`),
+        input: $(`<input class="ml-1 filter_item" type="radio" name="role" id="rb_Front-End" value="Front-End" />`)
+    };
 
-        item.input.change(() => { handleOnChange(role) });
-        inputs.push(item);
-    }
+    const showBackEnd = {
+        label: $(`<label class="m-0 col-6 p-0" for="rb_Back-End">Back-End</label>`),
+        input: $(`<input class="ml-1 filter_item" type="radio" name="role" id="rb_Back-End" value="Back-End" />`)
+    };
+
+    const showBoth = {
+        label: $('<label class="m-0 col-6 p-0" for="rb_show_both">Both</label>'),
+        input: $('<input class="ml-1 filter_item" type="radio" name="role" id="rb_show_both" value="both" />')
+    };
+
+    // Add Event Handlers to radio buttons
+    showAll.input.change(() => { handleOnChange('all') });
+    showFrontEnd.input.change(() => { handleOnChange('Front-End') });
+    showBackEnd.input.change(() => { handleOnChange('Back-End') });
+    showBoth.input.change(() => { handleOnChange('both') });
+
+    // Add list items to DOM
+    const inputs = [ showAll, showFrontEnd, showBackEnd, showBoth ];
 
     for (const item of inputs) {
-        const li = $('<li class="ml-1">');
+        const li = $('<li class="ml-1 mb-1 row align-items-center">');
         li.append(item.label, item.input);
         list.append(li);
     }
